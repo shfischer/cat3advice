4912d3ef2be2f56c8f3c7e4dda6022a63c3f5306
shfischer <32540427+shfischer@users.noreply.github.com>
2023-04-03 11:58:36 +0100

    added example data

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' @title Western English Channel plaice example data
+#'
+#' This data set contains 2022 example data from Western English Channel plaice (ple.27.7e) to illustrate the application of the ICES category 3 empirical harvest control rules.
+#'
+#'
+#' @source \url{https://github.com/ices-taf}
+#' @name ple7e
+NULL
+
+#' @rdname ple7e
+"ple7e_catch"
+
+#' @rdname ple7e
+"ple7e_f"
+
+#' @rdname ple7e
+"ple7e_f2"
+
+#' @rdname ple7e
+"ple7e_hr"
+
+#' @rdname ple7e
+"ple7e_idx"
+
